4f407d7b0ae915039bce5cb54c151ccd3835dd62
Milenakamine <aakaminemilena@gmail.com>
2020-06-16 15:23:29 -0300

    DojoFinaizado

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dojo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Jogador griezmann  = new Jogador();
+            griezmann.Nome= "Griezmann";
+            griezmann.Altura = 1.76f;
+            griezmann.Peso = 73f;
+            griezmann.Nascimento = DateTime.Parse("21/03/1991");
+            griezmann.Posicao = "Atacante";
+
+            Console.WriteLine(griezmann.CalcularIdade());
+            Console.WriteLine(griezmann.VerificarAposentadoria());
+        }
+    }
+}
